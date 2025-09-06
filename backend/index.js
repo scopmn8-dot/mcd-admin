65b7d3d7dd6e81dcbf9e602d2cdf6b8d2c92306a
scopmn8-dot <scopmn8@gmail.com>
2025-09-06 12:12:54 +0300

    Fix nodemailer typo: createTransport not createTransporter

--- backend/index.js
+++ backend/index.js
@@ -87,7 +87,7 @@ const EMAIL_CONFIG = {
 };
 
 // Create email transporter
-const emailTransporter = nodemailer.createTransporter(EMAIL_CONFIG);
+const emailTransporter = nodemailer.createTransport(EMAIL_CONFIG);
 
 // Email templates
 const EMAIL_TEMPLATES = {
